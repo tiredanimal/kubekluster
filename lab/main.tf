7fe0ac240b2c9ddaedecbf21ed0225abca7de15e
Matt Willsher <matt@monki.org.uk>
2021-02-01 20:50:23 +0000

    Add variables at top level

--- lab/main.tf
+++ lab/main.tf
@@ -8,18 +8,6 @@ terraform {
   }
 }
 
-locals {
-  controllers = {
-    memory = "2048"
-    count  = 3
-  }
-  workers = {
-    memory = "4096"
-    count  = 3
-  }
-  ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDy6EgOgmiyjhTQqVNDmqVF1u0ahvQUOf1zvgGaIDRMk matt@manjaro"
-}
-
 # instance the provider
 provider "libvirt" {
   uri = "qemu:///system"
@@ -41,12 +29,12 @@ resource "libvirt_volume" "debian" {
 module "controllers" {
   source = "./modules/node"
 
-  memory         = "2048"
-  number         = 1
+  memory         = tostring(var.controller_memory)
+  number         = var.controller_count
   name_prefix    = "control"
   network_offset = 16
 
-  ssh_public_key = local.ssh_public_key
+  ssh_public_key = var.ssh_public_key
 
   storage_pool_name = libvirt_pool.cluster.name
   base_volume_id    = libvirt_volume.debian.id
@@ -56,12 +44,12 @@ module "controllers" {
 module "workers" {
   source = "./modules/node"
 
-  memory         = "2048"
-  number         = 2
+  memory         = tostring(var.worker_memory)
+  number         = var.worker_count
   name_prefix    = "worker"
   network_offset = 32
 
-  ssh_public_key = local.ssh_public_key
+  ssh_public_key = var.ssh_public_key
 
   storage_pool_name = libvirt_pool.cluster.name
   base_volume_id    = libvirt_volume.debian.id
